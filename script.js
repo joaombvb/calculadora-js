4ea0a5f5b7a61ce29ed4cab27c12005ddfa9975a
João Marcelo Barros Vilas Boas <joaombvb@gmail.com>
2024-10-03 14:16:12 -0300

    <feat>(js): adiciona funcionalidade a operadores

--- script.js
+++ script.js
@@ -1,31 +1,93 @@
 let valor = 0;
-let botoes = document.querySelectorAll("button");
-let display = document.querySelector("p");
-display.textContent = valor;
+const botoes = document.querySelectorAll("button");
+const display = document.querySelector("p");
+let valorNoDisplay = "";
+let valorAnterior = 0;
+let resultado = 0;
+let operador = "";
+const operadores = ['+', '-', 'x', '÷'];
+display.textContent = "0";
 
 botoes.forEach(botao => { 
-    botao.addEventListener("click", 
-        function() {
-            valor = botao.textContent;
-            mostrarNumero();
+    botao.addEventListener("click", function() {
+        if (display.textContent === "0") {
+            display.textContent = "";
         }
-    );
+        valor = botao.textContent;
+        mostrarNumero();
+    });
 });
 
 function mostrarNumero() {
-    valor = parseInt(valor);
+    valorNoDisplay = display.textContent;
 
-    if (valor >= 0 && valor < 10) {
-
-        if (valor == 0) {
-            display.textContent = valor;
+    if ((parseInt(valor) >= 0 && parseInt(valor) <= 9)) {
+        valorNoDisplay += valor;
+        display.textContent = valorNoDisplay;
+    } else if (valor === ".") {
+        if (valorNoDisplay.includes(".")) {
+            return;
+        }
+        valorNoDisplay === "" ? valorNoDisplay = "0" + valor : valorNoDisplay += valor;
+        display.textContent = valorNoDisplay;
+    } else if (valor === "+/-") {
+        if (valorNoDisplay === "") {
+            display.textContent = "0";
         } else {
-            let valorNoDisplay = display.textContent;
-            valorNoDisplay = valorNoDisplay + valor;
+            valorNoDisplay.includes("-") ? valorNoDisplay = valorNoDisplay.replace(/-/, "") : valorNoDisplay = "-" + valorNoDisplay;
             display.textContent = valorNoDisplay;
         }
-    } else {
-        console.log("erro");
+    } else if (valor === "=") {
+        if (display.textContent === "") {
+            display.textContent = 0;
+        }
+        resultado = display.textContent;
+        calcular();
+        display.textContent = resultado;
+    } else if (valor === "C") {
+        limpar();
+    } else if (operadores.includes(valor)) {
+        if (valorNoDisplay === "") {
+            valorNoDisplay = 0;
+        }
+        valorAnterior = parseFloat(resultado);
+        operador = valor;
+        display.textContent = "0";
     }
 }
+
+function calcular() {
+    let valorAtual = valorNoDisplay;
+    valorAnterior = parseFloat(resultado);
+
+    if (valorAtual === "") {
+        valorAtual = 0;
+    } else {
+        valorAtual = parseFloat(valorAtual);
+    }
+
+    if (operador === "+") {
+        resultado = valorAnterior + valorAtual;
+    } else if (operador === "-") {
+        resultado = valorAnterior - valorAtual;
+    } else if (operador === "x") {
+        resultado = valorAnterior * valorAtual;
+    } else if (operador === "÷") {
+        if (valorAtual <= 0) {
+            limpar();
+            resultado = "ERROR";
+        } else {
+            resultado = valorAnterior / valorAtual;
+        }    
+    }
+}
+
+function limpar() {
+    display.textContent = "0";
+    valorAnterior = 0;
+    valorAtual = 0;
+    resultado =  0;
+    operador = "";  
+}
+
 
